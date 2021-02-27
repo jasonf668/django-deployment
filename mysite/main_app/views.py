e5fc9265162fd3e151430ca75c018eadb00e7740
jasonf668 <79561230+jasonf668@users.noreply.github.com>
2021-02-27 16:20:04 -0600

    Update views.py
    Removed Ads View

--- mysite/main_app/views.py
+++ mysite/main_app/views.py
@@ -8,11 +8,13 @@ import random
 from string import digits
 import math
 
+'''
 class AdsView(View):
     """Replace pub-0000000000000000 with your own publisher ID"""
     line  =  "google.com, pub-0000000000000000, DIRECT, f08c47fec0942fa0"
     def get(self, request, *args, **kwargs):
         return HttpResponse(line)
+'''
 
 # Create your views here.
 class IndexView(TemplateView):
