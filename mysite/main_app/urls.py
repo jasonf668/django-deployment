3f1fcadbc4d14216d360f6d9e00c8520dfe9b50d
jasonf668 <79561230+jasonf668@users.noreply.github.com>
2021-02-27 16:20:52 -0600

    Update urls.py
    Removed Ads url

--- mysite/main_app/urls.py
+++ mysite/main_app/urls.py
@@ -3,7 +3,7 @@ import main_app.views as views
 from .views import *
 
 urlpatterns = [
-    path('ads.txt', AdsView.as_view()),
+    #path('ads.txt', AdsView.as_view()),
     path('random_porn', views.random_porn),
     path('random_star', views.random_star),
     path('from_category', views.from_category),
